a0907e77cb833d60de3d144b42cbbaf2fbe86a23
CarlosArturoMT <carlosarturomt@gmail.com>
2021-08-30 13:15:53 -0500

    se agregan especificaciones de imc en js

--- public/js/dist/indice.dev.js
+++ public/js/dist/indice.dev.js
@@ -13,10 +13,69 @@ function indiceMasaC() {
   var weightValue = inputWeight.value;
   var inputHeight = document.getElementById("inputHeight");
   var heightValue = inputHeight.value;
-  var imc = indice(weightValue, heightValue);
   var resultIMC = document.getElementById("resultIMC");
-  resultIMC.innerText = "IMC: " + imc.toFixed(2);
-  resultIMC.classList.add('show');
+  var imc = indice(weightValue, heightValue);
+
+  switch (true) {
+    case imc > 0 && imc < 15:
+      resultIMC.innerText = "IMC: " + imc.toFixed(2);
+      resultIMC.classList.add('show', 'danger');
+      resultIMC1.innerText = "Bajo Peso Severo";
+      resultIMC1.classList.add('show', 'danger');
+      resultIMC2.innerText = "Riego Muy Severo";
+      resultIMC2.classList.add('show', 'danger');
+      break;
+
+    case imc > 15 && imc < 16:
+      resultIMC.innerText = "IMC: " + imc.toFixed(2);
+      resultIMC.classList.add('show', 'danger');
+      resultIMC1.innerText = "Bajo Peso Severo";
+      resultIMC1.classList.add('show', 'danger');
+      break;
+
+    case imc > 16.01 && imc < 18.5:
+      resultIMC.innerText = "IMC: " + imc.toFixed(2);
+      resultIMC.classList.add('show', 'careful');
+      resultIMC1.innerText = "Bajo Peso";
+      resultIMC1.classList.add('show', 'careful');
+      break;
+
+    case imc > 18.51 && imc < 24.9:
+      resultIMC.innerText = "IMC: " + imc.toFixed(2);
+      resultIMC.classList.add('show', 'normal');
+      resultIMC1.innerText = "Saludable";
+      resultIMC1.classList.add('show', 'normal');
+      break;
+
+    case imc > 25 && imc < 29.9:
+      resultIMC.innerText = "IMC: " + imc.toFixed(2);
+      resultIMC.classList.add('show', 'careful');
+      resultIMC1.innerText = "Sobrepeso";
+      resultIMC1.classList.add('show', 'careful');
+      break;
+
+    case imc > 30 && imc < 34.9:
+      resultIMC.innerText = "IMC: " + imc.toFixed(2);
+      resultIMC.classList.add('show', 'danger');
+      resultIMC1.innerText = "Obesidad Grado I";
+      resultIMC1.classList.add('show', 'danger');
+      break;
+
+    case imc > 35 && imc < 39.9:
+      resultIMC.innerText = "IMC: " + imc.toFixed(2);
+      resultIMC.classList.add('show', 'danger');
+      resultIMC1.innerText = "Obesidad Grado II";
+      resultIMC1.classList.add('show', 'danger');
+      break;
+
+    case imc > 40 && imc < 999:
+      resultIMC.innerText = "IMC: " + imc.toFixed(2);
+      resultIMC.classList.add('show', 'danger');
+      resultIMC1.innerText = "Obesidad Grado III";
+      resultIMC1.classList.add('show', 'danger');
+      resultIMC2.innerText = "Riego Muy Severo";
+      resultIMC2.classList.add('show', 'danger');
+  }
 }
 
 console.groupEnd();
--- public/js/indice.js
+++ public/js/indice.js
@@ -13,12 +13,73 @@ function indiceMasaC() {
   const inputHeight = document.getElementById("inputHeight");
   const heightValue = inputHeight.value;
 
-  const imc = indice(weightValue, heightValue);
-
   const resultIMC = document.getElementById("resultIMC");
 
-  resultIMC.innerText = "IMC: " + imc.toFixed(2);
-  resultIMC.classList.add('show');
+  const imc = indice(weightValue, heightValue);
+
+  switch(true){
+    case imc > 0 && imc < 15:
+      resultIMC.innerText = "IMC: " + imc.toFixed(2);
+      resultIMC.classList.add('show', 'danger');
+
+      resultIMC1.innerText = "Bajo Peso Severo";
+      resultIMC1.classList.add('show', 'danger');
+
+      resultIMC2.innerText = "Riego Muy Severo";
+      resultIMC2.classList.add('show', 'danger');
+      break;
+    case imc > 15 && imc < 16:
+      resultIMC.innerText = "IMC: " + imc.toFixed(2);
+      resultIMC.classList.add('show', 'danger');
+
+      resultIMC1.innerText = "Bajo Peso Severo";
+      resultIMC1.classList.add('show', 'danger');
+      break;
+    case imc > 16.01 && imc < 18.5:
+      resultIMC.innerText = "IMC: " + imc.toFixed(2);
+      resultIMC.classList.add('show', 'careful');
+
+      resultIMC1.innerText = "Bajo Peso";
+      resultIMC1.classList.add('show', 'careful');
+      break;
+    case imc > 18.51 && imc < 24.9:
+      resultIMC.innerText = "IMC: " + imc.toFixed(2);
+      resultIMC.classList.add('show', 'normal');
+
+      resultIMC1.innerText = "Saludable";
+      resultIMC1.classList.add('show', 'normal');
+      break;
+    case imc > 25 && imc < 29.9:
+      resultIMC.innerText = "IMC: " + imc.toFixed(2);
+      resultIMC.classList.add('show', 'careful');
+
+      resultIMC1.innerText = "Sobrepeso";
+      resultIMC1.classList.add('show', 'careful');
+      break;
+    case imc > 30 && imc < 34.9:
+      resultIMC.innerText = "IMC: " + imc.toFixed(2);
+      resultIMC.classList.add('show', 'danger');
+
+      resultIMC1.innerText = "Obesidad Grado I";
+      resultIMC1.classList.add('show', 'danger');
+      break;
+    case imc > 35 && imc < 39.9:
+      resultIMC.innerText = "IMC: " + imc.toFixed(2);
+      resultIMC.classList.add('show', 'danger');
+
+      resultIMC1.innerText = "Obesidad Grado II";
+      resultIMC1.classList.add('show', 'danger');
+      break;
+    case imc > 40 && imc < 999:
+      resultIMC.innerText = "IMC: " + imc.toFixed(2);
+      resultIMC.classList.add('show', 'danger');
+
+      resultIMC1.innerText = "Obesidad Grado III";
+      resultIMC1.classList.add('show', 'danger');
+
+      resultIMC2.innerText = "Riego Muy Severo";
+      resultIMC2.classList.add('show', 'danger');
+  }
 }
 
 console.groupEnd();
