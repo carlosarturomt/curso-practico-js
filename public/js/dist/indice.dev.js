5692add2b4b88a39b441f368b1c7159abdb90444
CarlosArturoMT <carlosarturomt@gmail.com>
2021-09-01 16:18:25 -0500

    pequeño bug arreglado, espero

--- public/js/dist/indice.dev.js
+++ public/js/dist/indice.dev.js
@@ -26,11 +26,12 @@ function indiceMasaC() {
       resultIMC2.classList.add('show', 'danger');
       break;
 
-    case imc > 15 && imc < 16:
+    case imc > 15.01 && imc < 16:
       resultIMC.innerText = "IMC: " + imc.toFixed(2);
       resultIMC.classList.add('show', 'danger');
       resultIMC1.innerText = "Bajo Peso Severo";
       resultIMC1.classList.add('show', 'danger');
+      resultIMC2.innerText = "Riego Moderado";
       break;
 
     case imc > 16.01 && imc < 18.5:
@@ -38,37 +39,42 @@ function indiceMasaC() {
       resultIMC.classList.add('show', 'careful');
       resultIMC1.innerText = "Bajo Peso";
       resultIMC1.classList.add('show', 'careful');
+      resultIMC2.innerText = "Riego Leve";
       break;
 
-    case imc > 18.51 && imc < 24.9:
+    case imc > 18.51 && imc < 24.99:
       resultIMC.innerText = "IMC: " + imc.toFixed(2);
       resultIMC.classList.add('show', 'normal');
       resultIMC1.innerText = "Saludable";
       resultIMC1.classList.add('show', 'normal');
+      resultIMC2.innerText = "Buen IMC";
       break;
 
-    case imc > 25 && imc < 29.9:
+    case imc > 25.01 && imc < 29.99:
       resultIMC.innerText = "IMC: " + imc.toFixed(2);
       resultIMC.classList.add('show', 'careful');
       resultIMC1.innerText = "Sobrepeso";
       resultIMC1.classList.add('show', 'careful');
+      resultIMC2.innerText = "Riego Leve";
       break;
 
-    case imc > 30 && imc < 34.9:
+    case imc > 30.01 && imc < 34.99:
       resultIMC.innerText = "IMC: " + imc.toFixed(2);
       resultIMC.classList.add('show', 'danger');
       resultIMC1.innerText = "Obesidad Grado I";
       resultIMC1.classList.add('show', 'danger');
+      resultIMC2.innerText = "Riego Moderado";
       break;
 
-    case imc > 35 && imc < 39.9:
+    case imc > 35.01 && imc < 39.99:
       resultIMC.innerText = "IMC: " + imc.toFixed(2);
       resultIMC.classList.add('show', 'danger');
       resultIMC1.innerText = "Obesidad Grado II";
       resultIMC1.classList.add('show', 'danger');
+      resultIMC2.innerText = "Riego Severo";
       break;
 
-    case imc > 40 && imc < 999:
+    case imc > 40.01 && imc < 999:
       resultIMC.innerText = "IMC: " + imc.toFixed(2);
       resultIMC.classList.add('show', 'danger');
       resultIMC1.innerText = "Obesidad Grado III";
